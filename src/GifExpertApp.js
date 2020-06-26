2b4305b901e988fb226f76c36fcedf856802ac8b
Felix Battig <fbattig@kreonovo.ca>
2020-06-26 10:25:15 -0400

    first commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+export const GifExpertApp = () => {
+	const [categories, setCategories] = useState(['love']);
+
+	return (
+		<div>
+			<h2> Gif expert app</h2>
+			<hr />
+			<AddCategory setCategories={setCategories} />
+			<ul>
+				{categories.map((category) => (
+					
+						<GifGrid key={category} category={category} />
+					
+				))}
+			</ul>
+		</div>
+	);
+};
