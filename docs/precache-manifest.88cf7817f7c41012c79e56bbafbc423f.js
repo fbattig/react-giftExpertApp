3b4139d7942571a9a3d23423c30ff42d2f9016ac
Felix Battig <fbattig@kreonovo.ca>
2020-06-26 10:32:54 -0400

    renamed build to docs

---
+++ docs/precache-manifest.88cf7817f7c41012c79e56bbafbc423f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8183127420e1633bca4945b0b15f975c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00c2cadc1a197d9d8213",
+    "url": "/static/css/main.2b2ae39e.chunk.css"
+  },
+  {
+    "revision": "3ec50c8fead662706b17",
+    "url": "/static/js/2.637c77a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.637c77a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00c2cadc1a197d9d8213",
+    "url": "/static/js/main.ba4572e5.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
